a191dd23f41f6c2173d3e03b3156e65e03c893f0
zhangyunfuh <18627600981@163.com>
2019-01-20 22:44:50 +0800

    nine

--- tpsPro/static/js/detailed.js
+++ tpsPro/static/js/detailed.js
@@ -24,6 +24,7 @@ $(function(){
 
 
 
+
 //放大镜
 
 	$("#smallPicBox").mousemove(function(e){
@@ -69,83 +70,4 @@ $(function(){
 		$("#bigPic2").css({"display":"block"});
 	})
 })
-// //商品规格
-// $(function(){
-// 	$("#guige").find("li").click(
-// 		function(){
-// 		    $(this).addClass("active2").siblings().removeClass("active2");
-// 	        console.log(this);
-// 	        var s=$(this);
-// 	        console.log(s);
-// 		}
-//     )
-// })
-// // //数量加减
-// // $(function(){
-// // 	var a=1;
-// // 	var q=true;
-// // 	$("#add").bind("selectstart",function(){return false;});
-// // 	$("#add").click(function(){
-// // 		a++;
-// // 		$("#shuliang").val(a);
-// //
-// // 	})
-// // 	$("#sub").bind("selectstart",function(){return false;});
-// // 	$("#sub").click(function(){
-// // 		a--;
-// // 		if(a<=0){
-// // 			a=0;
-// // 		}
-// // 		$("#shuliang").val(a);
-// // 	})
-// //直接输入要购买的商品数量
-// 	$("#shuliang").blur(function(){
-// 		a=$("#shuliang").val();
-// 	})
-// //购物车COOKICE
-// 	$("#gwc").click(function(){
-// 		var img=$(this).parent().parent().parent().find("div").eq(0).find("img").eq(0).attr("src");
-// 		var name=$(this).parent().parent().parent().find("#boxContent_r").find("h3").html();
-// 		var guige=$(this).parent().parent().parent().find("#boxContent_r").find("div").eq(1).children().children().eq(0).html();
-// 		var price=$(this).parent().parent().parent().find("#boxContent_r").find("span").eq(4).find("i").html();
-// 		var arra=$.cookie("cart") ? JSON.parse($.cookie("cart")):[];
-// 		var is=false;
-// 		for(var i=0;i<arra.length;i++){
-// 			if(arra[i].img1==img){
-// 				is=true;
-// 				arra[i].num+=a;
-// 			}
-// 		}
-// 		if(!is){
-// 			var obj={
-// 				img1:img,
-// 				name1:name,
-// 				guige1:guige,
-// 				price1:price,
-// 			    num:a
-// 		   }
-// 			arra.push(obj);
-// 		}
-// 		$.cookie("cart",JSON.stringify(arra),{expires:29,path:"/"});
-// 		console.log($.cookie("cart"));
-// 	})
-// 	$("#goumai").click(function(){
-// 		window.open("cart.html","_blank");
-// 	})
-// 	$("#check").click(function(){
-// 		window.open("cart.html","_blank");
-// 	})
-// })
-// //添加购物车弹出窗口
-// $(function(){
-// 	$("#gwc").click(function(){
-// 		$("#addSuccess").fadeIn(100);
-// 	})
-// 	$("#continue").click(function(){
-// 		$("#addSuccess").fadeOut(1);
-// 	})
-// })
-// //购物车COOKICE
-// //$(function(){
-// //
-// //})
+
