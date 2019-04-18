6614e29510d136fc061867cf391fa348ce199e8f
zhangyunfuh <18627600981@163.com>
2019-04-18 17:34:43 +0800

    第4次修改

--- tpsPro/tpsapp/models.py
+++ tpsPro/tpsapp/models.py
@@ -1,31 +1,36 @@
 from django.db import models
 
+
 class User(models.Model):
-    name=models.CharField(max_length=50)
-    password=models.CharField(max_length=100)
-    token=models.CharField(max_length=100)
-    auth=models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    token = models.CharField(max_length=100)
+    auth = models.CharField(max_length=10)
 
 
 class wheel(models.Model):
-    img=models.CharField(max_length=200)
-    trackid=models.CharField(max_length=30)
+    img = models.CharField(max_length=200)
+    trackid = models.CharField(max_length=30)
+
     class Meta:
-        db_table='wheel'
+        db_table = 'wheel'
+
 
 class Detail(models.Model):
-    title=models.CharField(max_length=200)
-    addr=models.CharField(max_length=20)
-    price=models.CharField(max_length=10)
-    img=models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    addr = models.CharField(max_length=20)
+    price = models.CharField(max_length=10)
+    img = models.CharField(max_length=200)
+
     class Meta:
-        db_table='tpsapp_detail'
+        db_table = 'tpsapp_detail'
 
 
 class Cart(models.Model):
-    user=models.ForeignKey(User)
-    products=models.ForeignKey(Detail)
-    number=models.IntegerField()
-    isselect=models.BooleanField(default=True)
+    user = models.ForeignKey(User)
+    products = models.ForeignKey(Detail)
+    number = models.IntegerField()
+    isselect = models.BooleanField(default=True)
+
     class Meta:
-        db_table='tpsapp_cart'
+        db_table = 'tpsapp_cart'
--- tpsPro/tpsapp/urls.py
+++ tpsPro/tpsapp/urls.py
@@ -2,20 +2,20 @@ from django.conf.urls import url
 from tpsapp import views
 from tpsapp import yanzhengma
 
-urlpatterns=[
-    url(r'^$',views.homepage,name='homepage'),
-    url(r'^login/$',views.login,name='login'),
-    url(r'^register/$',views.register,name='register'),
-    url(r'^logout/$',views.logout,name='logout'),
-    url(r'^detailed/(\d+)/$',views.detailed,name='detailed'),
+urlpatterns = [
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^detailed/(\d+)/$', views.detailed, name='detailed'),
     # url(r'^detailed2/$',views.detailed2,name='detailed2'),
-    url(r'^cart/$',views.cart,name='cart'),
-    url(r'^checkuser/$',views.checkuser,name='checkuser'),
-    url(r'^addcart/$',views.addcart,name='addcart'),
-    url(r'^appnotify/$',views.appnotify,name='appnotify'),
-    url(r'^returnview/$',views.returnview,name='returnview'),
-    url(r'^pay/$',views.pay,name='pay'),
-    url(r'^delete/(\d+)/$',views.delete,name='delete'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^checkuser/$', views.checkuser, name='checkuser'),
+    url(r'^addcart/$', views.addcart, name='addcart'),
+    url(r'^appnotify/$', views.appnotify, name='appnotify'),
+    url(r'^returnview/$', views.returnview, name='returnview'),
+    url(r'^pay/$', views.pay, name='pay'),
+    url(r'^delete/(\d+)/$', views.delete, name='delete'),
     # url(r'^get_verify_img/$',views.get_verify_img,name='get_verify_img')
     url(r'^yanzhengma/$', yanzhengma.get_valid_img, name='yanzhengma'),
 
