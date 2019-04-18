8098ead0679f4e02d1319995a4bce9732b84a219
zhangyunfuh <18627600981@163.com>
2019-04-18 16:37:20 +0800

    第3次修改

--- tpsPro/tpsapp/views.py
+++ tpsPro/tpsapp/views.py
@@ -8,79 +8,85 @@ from tpsapp.models import User, wheel, Detail, Cart
 from django.http import HttpResponse,HttpRequest
 
 
-#首页
+# 首页
 def homepage(request):
-    token=request.session.get('token')
-    users=User.objects.filter(token=token)
+    token = request.session.get('token')
+    users = User.objects.filter(token=token)
     # 判断是否是登录状态
     if users.count():
-        user=users.first()
-        name=user.name
+        user = users.first()
+        name = user.name
     else:
-        name=None
-    #得到商品详情的数据对象comp传到首页的模板，为了获得商品id 区分每一个商品
+        name = None
+    # 得到商品详情的数据对象comp传到首页的模板，为了获得商品id 区分每一个商品
     comp=Detail.objects.all()
     imgs=wheel.objects.all()
     carts = Cart.objects.all()
-    return render(request,'homepage.html',{'name':name,'comps':comp,'imgs':imgs,'carts':carts})
+    return render(request,'homepage.html', {'name': name, 'comps': comp, 'imgs': imgs, 'carts': carts})
+
 
 # 生成唯一标识符token
 def generate_token():
-    token =str(time.time()) + str(random.random())
-    md5=hashlib.md5()
+    token = str(time.time()) + str(random.random())
+    md5 = hashlib.md5()
     md5.update(token.encode('utf-8'))
     return md5.hexdigest()
 
-#生成加密的密码
+
+# 生成加密的密码
 def generate_password(passwd):
     sha = hashlib.sha256()
     sha.update(passwd.encode('utf-8'))
     return sha.hexdigest()
 
-#注册
+
+# 注册
 def register(request):
-    #如果是post请求则把request的数据保存到数据库
-    if request.method=='GET':
+    # 如果是post请求则把request的数据保存到数据库
+    if request.method == 'GET':
         return render(request,'login.html')
-    elif request.method=='POST':
-        user=User()
-        user.name=request.POST.get('name')
-        user.password=generate_password(request.POST.get('password'))
+    elif request.method == 'POST':
+        user = User()
+        user.name = request.POST.get('name')
+        user.password = generate_password(request.POST.get('password'))
         user.token = generate_token()
         user.save()
-        response=redirect('tpsapp:homepage')
-        request.session['token']=user.token
+        response = redirect('tpsapp:homepage')
+        request.session['token'] = user.token
         request.session.set_expiry(0)
         return response
 
-#登录
+
+# 登录
 def login(request):
-    #render的作用就是将数据填充进模板文件，最后把结果返回给浏览器
-    if request.method=='GET':
-        return render(request,'login.html')
-    elif request.method=='POST':
-        #获得post请求的数据
-        name=request.POST.get('name')
-        password=generate_password(request.POST.get('password'))
-        #判断用户数据在数据库是否有匹配
-        users=User.objects.filter(name=name).filter(password=password)
+    # render的作用就是将数据填充进模板文件，最后把结果返回给浏览器
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        # 获得post请求的数据
+        name = request.POST.get('name')
+        password = generate_password(request.POST.get('password'))
+        # 判断用户数据在数据库是否有匹配
+        users = User.objects.filter(name=name).filter(password=password)
         if users.count():
-            user=users.first()
+            user = users.first()
             response = redirect('tpsapp:homepage')
-            request.session['token']=user.token
+            request.session['token'] = user.token
             request.session.set_expiry(0)
             return response
         else:
-            err='用户名或密码错误'
+            err = '用户名或密码错误'
             return render(request,'login.html',{'err':err})
 
-#注销
+
+# 注销
 def logout(request):
-    response=redirect('tpsapp:homepage')
+    response = redirect('tpsapp:homepage')
     request.session.flush()
     return response
 
-#详情页，首页comp传过来的id=proid
+
+# 详情页，首页comp传过来的id=proid
 def detailed(request,proid):
     token = request.session.get('token')
     users = User.objects.filter(token=token)
@@ -89,11 +95,12 @@ def detailed(request,proid):
         name = user.name
     else:
         name = None
-    detail=Detail.objects.get(id=proid)
-    carts=Cart.objects.all()
-    return render(request, 'detailed.html', {'name': name,'detail':detail,'carts':carts})
+    detail = Detail.objects.get(id=proid)
+    carts = Cart.objects.all()
+    return render(request, 'detailed.html', {'name': name, 'detail': detail, 'carts': carts})
 
-#删除商品
+
+# 删除商品
 def delete(request,proid):
     token = request.session.get('token')
     users = User.objects.filter(token=token)
@@ -102,7 +109,8 @@ def delete(request,proid):
         cart.delete()
         return redirect('tpsapp:cart')
 
-#购物车操作
+
+# 购物车操作
 def cart(request):
     token = request.session.get('token')
     users = User.objects.filter(token=token)
@@ -111,21 +119,20 @@ def cart(request):
         name = user.name
     else:
         name = None
-        user=None
+        user = None
     # carts=Cart.objects.all()
     carts = Cart.objects.filter(user=user)
-    idten=random.randrange(10000,10000000)
-    return render(request,'cart.html',{'name':name,'carts':carts,'idtens':idten})
+    idten = random.randrange(10000,10000000)
+    return render(request, 'cart.html', {'name': name, 'carts': carts, 'idtens': idten})
 
 
 def checkuser(request):
-    username=request.GET.get('username')
-    users=User.objects.filter(name=username)
+    username = request.GET.get('username')
+    users = User.objects.filter(name=username)
     if users.exists():
-        return JsonResponse({'msg': '账号被占用','status':0})
+        return JsonResponse({'msg': '账号被占用', 'status': 0})
     else:
-
-        return JsonResponse({'msg': '可以使用！','status':1})
+        return JsonResponse({'msg': '可以使用！', 'status': 1})
 #
 # def detailed2(request):
 #     token = request.session.get('token')
@@ -137,36 +144,38 @@ def checkuser(request):
 #         name = None
 #     return render(request, 'detailed2.html', {'name': name})
 
+
+# 购物车的业务逻辑操作
 def addcart(request):
-    token=request.session.get('token')
+    token = request.session.get('token')
     print(token)
     if token:
-        users=User.objects.get(token=token)
-        detailid=request.GET.get('detailid')
-        details=Detail.objects.get(id=detailid)
-        carts=Cart.objects.filter(user=users).filter(products=details)
+        users = User.objects.get(token=token)
+        detailid = request.GET.get('detailid')
+        details = Detail.objects.get(id=detailid)
+        carts = Cart.objects.filter(user=users).filter(products=details)
         print(carts)
         if carts.exists():
-            cart=carts.first()
+            cart = carts.first()
             cart.number=request.GET.get('numb')
             cart.save()
-            msg={'msg':'{}-修改购物车成功'.format(details.title),'number':cart.number,'status':1}
+            msg = {'msg': '{}-修改购物车成功'.format(details.title), 'number': cart.number, 'status': 1}
             return JsonResponse(msg)
         else:
             carts = Cart()
             carts.user = users
             carts.products = details
-            carts.number=request.GET.get('numb')
-            carts.isselect=True
+            carts.number = request.GET.get('numb')
+            carts.isselect = True
             carts.save()
-            msg={'msg':'{}-添加购物车成功'.format(details.title),'number':carts.number,'status':1,'cishu':0}
+            msg = {'msg': '{}-添加购物车成功'.format(details.title), 'number': carts.number, 'status': 1, 'cishu': 0}
             return JsonResponse(msg)
     else:
         return JsonResponse({'msg':'登入后购买','status':0})
 
 
 def appnotify(request):
-    return JsonResponse({'msg':'success'})
+    return JsonResponse({'msg': 'success'})
 
 
 def returnview(request):
@@ -174,18 +183,19 @@ def returnview(request):
 
 
 def pay(request):
-    ident=request.GET.get('ident')
-    totals=request.GET.get('total')
-    alipayurl=alipay.direct_pay(
+    ident = request.GET.get('ident')
+    totals = request.GET.get('total')
+    alipayurl = alipay.direct_pay(
         subject='book-2018',
         out_trade_no=ident,
         total_amount=totals,
         return_url='http://47.106.251.205/returnview/'
     )
-    alipayurl='https://openapi.alipaydev.com/gateway.do?{data}'.format(data=alipayurl)
-    return  JsonResponse({'alipayurl':alipayurl,'status':1})
+    alipayurl = 'https://openapi.alipaydev.com/gateway.do?{data}'.format(data=alipayurl)
+    return JsonResponse({'alipayurl': alipayurl, 'status': 1})
 
-#验证码
+
+# 验证码
 def login_api(req):
     if req.method == 'GET':
         return render(req, 'login.html')
